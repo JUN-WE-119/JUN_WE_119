68d8b537d888e4270b915cc6f3dd9f6a5cea2530
Kavindu_Lakshan_Perera <99538119+KavinduLakshanDev@users.noreply.github.com>
2024-05-11 22:17:27 +0530

    signup model

--- backend/src/main/java/com/paf/socialmedia/dto/SignupDTO.java
+++ backend/src/main/java/com/paf/socialmedia/dto/SignupDTO.java
@@ -1,3 +1,16 @@
+// package com.paf.socialmedia.dto;
+
+// import lombok.Getter;
+// import lombok.Setter;
+
+// @Getter
+// @Setter
+// public class SignupDTO {
+//     private String username;
+//     private String password;
+// }
+
+
 package com.paf.socialmedia.dto;
 
 import lombok.Getter;
