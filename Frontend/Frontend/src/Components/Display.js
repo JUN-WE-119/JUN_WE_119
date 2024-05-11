7566865e8cb551c0ad6f35558d1389cce959c703
IT21314742 <it21314742@my.sliit.lk>
2024-05-11 22:06:06 +0530

    Update
    Update

--- Frontend/Frontend/src/Components/Display.js
+++ Frontend/Frontend/src/Components/Display.js
@@ -2,7 +2,7 @@ import React,  { useState, useEffect } from 'react';
 import {useTable} from 'react-table';
 
 
-
+import
 // import EditUserModal from './EditUserModal'; // Assuming you have this modal component
 
 function UserForm() {
