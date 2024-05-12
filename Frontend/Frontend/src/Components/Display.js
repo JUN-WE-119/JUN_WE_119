7915becc13bb768d12a5fcc23b4a8f713c65e79a
IT21314742 <it21314742@my.sliit.lk>
2024-05-12 07:58:47 +0530

    Update
    Update

--- Frontend/Frontend/src/Components/Display.js
+++ Frontend/Frontend/src/Components/Display.js
@@ -14,7 +14,7 @@ function UserForm() {
 
     const fetchUsers = async () => {
         try {
-            const
+            const response
         }
     }
     // const fetchUsers = async () => {
